44f163f9625ef594550124ba3b70f7b09d1746aa
mohammad-ishtiaque <mohammad.ishtiaque833@gmail.com>
2025-06-26 15:30:36 +0600

    search method is implemented

--- src/controllers/Dashboard/UserManagement.controller.js
+++ src/controllers/Dashboard/UserManagement.controller.js
@@ -4,12 +4,43 @@ const User = require('../../models/User'); // adjust path as needed
  * Controller to list all registered users.
  */
 exports.getAllUsers = async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+    const search = req.query.search || '';
+
     try {
-        const users = await User.find(); // Retrieves all user documents from the database.
+        const query = {};
+        if (search) {
+            query.$or = [
+                { firstName: { $regex: search, $options: 'i' } },
+                { lastName: { $regex: search, $options: 'i' } },
+                { email: { $regex: search, $options: 'i' } }
+            ];
+        }
+
+        const users = await User.find(query, { password: 0 })
+            .skip(skip)
+            .limit(limit);
+
+        const count = await User.countDocuments(query);
+
+        if (users.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: 'No users found!'
+            });
+        }
         res.status(200).json({
             success: true,
             message: 'Users fetched successfully!',
-            data: users
+            data: users,
+            pagination: {
+                currentPage: page,
+                totalPages: Math.ceil(count / limit),
+                totalItems: count,
+                itemsPerPage: limit
+            }
         });
     } catch (error) {
         res.status(500).json({
