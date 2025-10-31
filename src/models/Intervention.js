a0c8e3b9372866cc6cabad49c88ec78a650b76d0
mohammad-ishtiaque <mohammad.ishtiaque833@gmail.com>
2025-10-31 14:09:48 +0600

    changes the intervention

--- src/models/Intervention.js
+++ src/models/Intervention.js
@@ -8,7 +8,7 @@ const imageSchema = new mongoose.Schema({
 });
 
 const interventionSchema = new mongoose.Schema({
-  interventionId: { type: String, unique: true, required: true },
+  interventionId: { type: String, required: true },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
   price: { type: Number, required: true },
